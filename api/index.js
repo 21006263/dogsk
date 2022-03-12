003116b8f3913d3036ca1bd5f366dbab77cfea1f
Nicolás Roldán <you@example.com>
2022-03-12 18:13:29 -0300

    cambios

--- api/index.js
+++ api/index.js
@@ -21,10 +21,11 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
+let port = process.env.PORT || 3001;
 conn.sync({ force: true }).then(() => {
 
   
-  server.listen(process.env.PORT, () => {
+  server.listen(port, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
