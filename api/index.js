46a89aa57d3d4c79eaf6374f4957075f528e92bd
Nicolás Roldán <you@example.com>
2022-03-12 19:30:10 -0300

    cambios

--- api/index.js
+++ api/index.js
@@ -21,11 +21,11 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-let port = process.env.PORT || 3001;
+
 conn.sync({ force: true }).then(() => {
 
   
-  server.listen(port, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import axios from 'axios';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import  store  from "./store";
 import { BrowserRouter } from 'react-router-dom'
-import  dotenv from "dotenv";
-import axios from 'axios';
+import dotenv from "dotenv";
 dotenv.config();
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+
 
 
 ReactDOM.render(
