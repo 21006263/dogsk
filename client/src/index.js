9cab9216c26b17a1d598823864b71c21d719f8b7
Henry <hmorenosifontes@gmail.com>
2022-03-25 13:52:53 -0400

    Pruebas con key

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
-import axios from 'axios';
 import  store  from "./store";
 import { BrowserRouter } from 'react-router-dom'
 import  dotenv from "dotenv";
+import axios from 'axios';
  dotenv.config();
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+ axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+
 
 
 ReactDOM.render(
