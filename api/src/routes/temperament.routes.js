a58248f55fb28d88369483e4644067a4aa67c7ac
Henry <hmorenosifontes@gmail.com>
2022-03-25 16:23:12 -0400

    cambios

--- api/src/routes/temperament.routes.js
+++ api/src/routes/temperament.routes.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const  express = require('express');
+const router = express.Router();
 
-const axios = require ('axios'); 
+const axios = require('axios'); 
 
 const {
     API_KEY
